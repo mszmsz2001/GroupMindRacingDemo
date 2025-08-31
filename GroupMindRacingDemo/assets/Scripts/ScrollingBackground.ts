46a20e245138576f69ae58e5514ca837f3868559
DengFenglai <1316165876@qq.com>
2025-08-31 15:53:45 +0800

    【功能】赛道逻辑

--- GroupMindRacingDemo/assets/Scripts/ScrollingBackground.ts
+++ GroupMindRacingDemo/assets/Scripts/ScrollingBackground.ts
@@ -11,7 +11,10 @@ export class ScrollingBackground extends Component {
     bg01: Node = null;
     @property(Node)
     bg02: Node = null;
+    @property(Node)
     bg03: Node = null;
+    @property(Node)
+    bg04: Node = null;
 
     // 不再需要 @property，因为它将由父节点控制
     // 但它仍然是一个 public 变量，外部可以访问和修改
@@ -31,13 +34,13 @@ export class ScrollingBackground extends Component {
         // this.canvasBottomY = -visibleHeight / 2; // 计算画布底部的Y坐标
         */
 
-        this.bg03 = this.bg01;
-
         // 设置初始位置,防止摆放错误
         this.bg00.setPosition(0, this.canvasBottomY + this.bgHeight/2);
         this.bg01.setPosition(0, this.canvasBottomY + this.bgHeight + this.bgHeight/2);
         this.bg02.setPosition(0, this.canvasBottomY + this.bgHeight*2 + this.bgHeight/2);
         this.bg03.setPosition(0, this.canvasBottomY + this.bgHeight*3 + this.bgHeight/2);
+        this.bg04.setPosition(0, this.canvasBottomY + this.bgHeight*4 + this.bgHeight/2);
+
     }
 
     update(deltaTime: number) {
@@ -46,31 +49,37 @@ export class ScrollingBackground extends Component {
         let position1 = this.bg01.position;
         let position2 = this.bg02.position;
         let position3 = this.bg03.position;
+        let position4 = this.bg04.position;
+
         // 计算背景移动距离
         let distance = this.bgSpeed * deltaTime;
         this.bg00.setPosition(position0.x, position0.y - distance);
         this.bg01.setPosition(position1.x, position1.y - distance);
         this.bg02.setPosition(position2.x, position2.y - distance);
         this.bg03.setPosition(position3.x, position3.y - distance);
+        this.bg04.setPosition(position4.x, position4.y - distance);
 
         // 不用上面的position是因为会有一帧的差值
         let p0 = this.bg00.position;
         let p1 = this.bg01.position;
         let p2 = this.bg02.position;
         let p3 = this.bg03.position;
+        let p4 = this.bg04.position;
+        
         // 如果背景移动出屏幕，则跳到下一个循环的位置
-        if (p0.y + this.bgHeight/2 < this.canvasBottomY) {
-            this.bg00.setPosition(p0.x, p0.y + this.bgHeight*3);
+        if (p4.y + this.bgHeight/2 < this.canvasBottomY) {
+            this.bg04.setPosition(p4.x, p4.y + this.bgHeight*4);
         }
         if (p1.y + this.bgHeight/2 < this.canvasBottomY) {
-            this.bg01.setPosition(p1.x, p1.y + this.bgHeight*3);
+            this.bg01.setPosition(p1.x, p1.y + this.bgHeight*4);
         }
         if (p2.y + this.bgHeight/2 < this.canvasBottomY) {
-            this.bg02.setPosition(p2.x, p2.y + this.bgHeight*3);
+            this.bg02.setPosition(p2.x, p2.y + this.bgHeight*4);
         }
         if (p3.y + this.bgHeight/2 < this.canvasBottomY) {
-            this.bg03.setPosition(p3.x, p3.y + this.bgHeight*3);
+            this.bg03.setPosition(p3.x, p3.y + this.bgHeight*4);
         }
+
     }
 }
 
